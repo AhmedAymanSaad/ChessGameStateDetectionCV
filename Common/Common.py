927220b328e9b4169d68ddec66754e4c47c56448
Ahmed Ayman Saad <34175435+AhmedAymanSaad@users.noreply.github.com>
2022-12-01 23:46:20 +0200

    Added Definitions, configuration and common function files

---
+++ Common/Common.py
@@ -0,0 +1,6 @@
+"""
+This class is responsible for the common functions used in the project.
+This includes all the image processing functions. 
+"""
+
+import numpy as np
---
+++ Common/Definitions.py
@@ -0,0 +1,17 @@
+"""
+This file contains all the static definitions pretianing to the project
+"""
+
+squareColors = { 0: "white", 1: "black" }
+pieceColors = { 0: "white", 1: "black" }
+pieceTypes = { 0: "empty", 1: "pawn", 2: "knight", 3: "bishop", 4: "rook", 5: "queen", 6: "king" }
+pieceTypesInv = { "empty": 0, "pawn": 1, "knight": 2, "bishop": 3, "rook": 4, "queen": 5, "king": 6 }
+pieceTypesShort = { 0: "e", 1: "p", 2: "n", 3: "b", 4: "r", 5: "q", 6: "k" }
+
+# board enum
+boardrows = { "a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7 }
+boardcols = { "1": 0, "2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7 }
+
+# ratios enum
+aspectRatios = { "square": 1, "pawn": 1, "knight": 1.25, "bishop": 1.5, "rook": 0.5, "queen": 0.5, "king": 0.5 }
+
