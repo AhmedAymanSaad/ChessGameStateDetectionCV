2d0ca067da2c9d22f182ff500c23ba24fd0ef016
Ahmed Ayman Saad <34175435+AhmedAymanSaad@users.noreply.github.com>
2022-12-01 23:47:07 +0200

    Created boarddetection file

---
+++ BoardDetection/BoardDetection.py
@@ -0,0 +1,19 @@
+"""
+This class is responsible for detecting the board and segments it into squares.
+"""
+import ChessBoard
+
+def detectBoard(chessBoard: ChessBoard) -> ChessBoard:
+    """
+    This function is responsible for detecting the board.
+    :return: The detected board.
+    """
+    pass
+
+def segmentBoard(chessBoard: ChessBoard) -> ChessBoard:
+    """
+    This function is responsible for segmenting the board into squares.
+    :return: The segmented board.
+    """
+    pass
+
