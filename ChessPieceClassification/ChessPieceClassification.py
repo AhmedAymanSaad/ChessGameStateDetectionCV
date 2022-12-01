5350530dca1170a2546eeadfbe85fcbadcb66414
Ahmed Ayman Saad <34175435+AhmedAymanSaad@users.noreply.github.com>
2022-12-01 23:47:30 +0200

    Created Classifier files and its data storage

---
+++ ChessPieceClassification/ChessPieceClassification.py
@@ -0,0 +1,18 @@
+"""
+"""
+import ChessBoard
+
+def classifySquares(board: ChessBoard)-> ChessBoard:
+    """
+    This function is responsible for classifying the squares.
+    :param board: The board to classify the squares from.
+    :return: The classified squares.
+    """
+    pass
+
+def trainSVMPieceClassifier():
+    """
+    This function is responsible for training the SVM piece classifier.
+    :return: The trained SVM piece classifier.
+    """
+    pass
